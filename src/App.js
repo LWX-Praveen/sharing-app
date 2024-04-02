9503b93761b23559c65b9b1982dc41adefaa8837
Praveen <praveen@Praveens-MacBook-Pro.local>
2024-04-02 17:14:40 -0400

    testing changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ImageShareComponent from './ImageShare';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <ImageShareComponent pageURL={"http://localhost:3000/post?id=1"} />
+       <ImageShareComponent pageURL={"http://localhost:3000/post?id=2"} />
       </header>
     </div>
   );
