4fef9a1160f78f66540cb74be3036a4f25a14c6b
Praveen <praveen@Praveens-MacBook-Pro.local>
2024-04-12 14:54:39 -0400

    Image changed

--- server/stub/posts.js
+++ server/stub/posts.js
@@ -2,12 +2,12 @@ const posts = [
     {
         title: "Post #1",
         description: "This is the first post",
-        thumbnail: "https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80"
+        thumbnail: "https://cdn.pokedx.app/0X43A0C5EB1763A211AA3C05849A617F2EE0452767.png"
     },
     {
         title: "Post #2",
         description: "This is the second post",
-        thumbnail: "https://images.unsplash.com/photo-1625034712314-7bd692b60ecb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+        thumbnail: "https://cdn.pokedx.app/0XB27ADAFFB9FEA1801459A1A81B17218288C097CC.png"
     },
     {
         title: "Post #3",
