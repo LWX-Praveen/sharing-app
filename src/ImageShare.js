e694f4767449094e39a0bf1fca2d3c928c90a9ce
Praveen <praveen@Praveens-MacBook-Pro.local>
2024-04-12 13:51:29 -0400

    Added linkedin

--- src/ImageShare.js
+++ src/ImageShare.js
@@ -10,11 +10,16 @@ function ImageShareComponent({  pageURL }) {
         window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(pageURL)}`, '_blank');
     }
 
+    const shareOnLinkedIn = () => {
+        window.open(`https://linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(pageURL)}`, '_blank')
+    }
+
     return (
         <div>
            
             <button onClick={shareOnFacebook}>Share on Facebook</button> &nbsp;
             <button onClick={shareOnTwitter}>Share on Twitter</button>
+            <button onClick={shareOnLinkedIn}>Share on Linkedin</button>
         </div>
     );
 }
